a121a1264619252982f588608c67d6640db4cdd0
Dokalenko <doroshenko.andrei@inbox.ru>
2016-12-29 23:11:10 +0200

    Update TitlesFrame.java
    make update of code

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -1,32 +1,28 @@
 /*
  * Decompiled with CFR 0_118.
  */
-import java.awt.Component;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-public class TitlesFrame /*main method*/
+class TitlesFrame /*main method*/
 extends JFrame {
-    public TitlesFrame() {
+    private TitlesFrame() {
         this.initUI();
     }
 
     private void initUI() {
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
-        this.add(new TitlesPanel(78));
+        this.add(new TitlesPanel());
         this.setSize(350, 350);
         this.setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable(){
-
-            @Override /*override*/
-            public void run() {
-                TitlesFrame ps = new TitlesFrame();
-                ps.setVisible(true);
-            }
+        /*override*/
+        SwingUtilities.invokeLater(() -> {
+            TitlesFrame ps = new TitlesFrame();
+            ps.setVisible(true);
         });
     }
 
