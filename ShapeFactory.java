c10760965fcb09b079bb3848240181ec0f9603ff
Dokalenko <doroshenko.andrei@inbox.ru>
2016-12-29 22:57:03 +0200

    Update ShapeFactory.java
    add coments

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -12,14 +12,16 @@ import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
-public class ShapeFactory {
+public class ShapeFactory /*setting*/
+{
     public Shape shape;
     public BasicStroke stroke = new BasicStroke(3.0f);
     public Paint paint;
     public int width = 25;
     public int height = 25;
 
-    public ShapeFactory(int shape_type) {
+    public ShapeFactory(int shape_type)/*figures type*/
+    {
         switch (shape_type = 1) {
             case 1: {
                 this.shape = ShapeFactory.createStar(3, new Point(0, 0), (double)this.width / 2.0, (double)this.width / 2.0);
